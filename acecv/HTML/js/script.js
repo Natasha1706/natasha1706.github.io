3189e999465e2c9d49ba8ab9ed1a1c7ee41b4a33
Natasha1706 <32267194+Natasha1706@users.noreply.github.com>
2018-05-16 14:40:40 +0300

    init
    init

---
+++ acecv/HTML/js/script.js
@@ -0,0 +1,34 @@
+// var modal2 = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function() {
+// 	modal2.style.display = "block";
+// }
+// span.onclick = function() {
+// 	modal2.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+// 	if (event.target == modal2){
+// 		modal2.style.display = "none";
+// 	}
+// }
+
+
+// var modal1 = document.getElementById("myModal1");
+// var btn1 = document.getElementById("myBtn1");
+// var span1 = document.getElementsByClassName("close")[1];
+
+
+// btn1.onclick = function() {
+// 	modal1.style.display = "block";
+// }
+// span1.onclick = function() {
+// 	modal1.style.display = "none";
+// }
+// window.onclick = function(event) {
+// 	if (event.target == modal1){
+// 		modal1.style.display = "none";
+// 	}
+// }
